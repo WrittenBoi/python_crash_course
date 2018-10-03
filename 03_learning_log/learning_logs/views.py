68206f5bfbeb779d0e36ba164bb0c6afbacc3254
Jack Chao <f8915345@gmail.com>
2018-10-03 21:10:56 +0800

    [03] Added topics and entries pages

--- 03_learning_log/learning_logs/views.py
+++ 03_learning_log/learning_logs/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Topic, Entry
 
 # Create your views here.
+
+def index(request):
+    """Main page of learning log"""
+    return render(request, "learning_logs/index.html")
+
+def topics(request):
+    """View all topics"""
+    topics = Topic.objects.order_by("date_added")
+    context = {"topics" : topics}
+    return render(request, "learning_logs/topics.html", context)
+
+def topic(request, topic_id):
+    """View a topic"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by("-date_added")
+    context = {"topic": topic, "entries": entries}
+    return render(request, "learning_logs/topic.html", context)
