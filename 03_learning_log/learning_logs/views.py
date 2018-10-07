c3a5b51ca369f7f86f228776743bbe1390e199f0
Jack Chao <f8915345@gmail.com>
2018-10-07 21:13:31 +0800

    [03] Added edit entry page

--- 03_learning_log/learning_logs/views.py
+++ 03_learning_log/learning_logs/views.py
@@ -53,3 +53,20 @@ def new_entry(request, topic_id):
 
     context = {'topic': topic, 'form': form}
     return render(request, "learning_logs/new_entry.html", context)
+
+def edit_entry(request, entry_id):
+    """Edit an entry"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if(request.method != 'POST'):
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if(form.is_valid()):
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',
+                                                    args=[topic.id]))
+
+    context = {'form': form, 'entry': entry, 'topic': topic}
+    return render(request, "learning_logs/edit_entry.html", context)
